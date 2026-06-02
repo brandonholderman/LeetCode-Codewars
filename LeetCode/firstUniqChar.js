e27f23f0e989c099d652ab051ad05dfd030e58e5
unknown <18756859+brandonholderman@users.noreply.github.com>
2026-06-01 22:58:09 -0700

    commit before branch change for new problem

--- LeetCode/firstUniqChar.js
+++ LeetCode/firstUniqChar.js
@@ -2,6 +2,90 @@
 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 */
 
+TODO: // Implement MinHeap method for increased performance. Formula is (index - 1) / 2. This returns the root or the "least" value.
+// let parent = Math.floor((i - 1) / 2)
+// let leftChild = 2 * i + 1
+// let rightChild = 2 * i + 2
+
+
+function MinHeap() {
+    let heap = []
+
+    function getParent(i) {
+        return Math.floor((i - 1) / 2)
+    }
+
+    function getLeft(i) {
+        return ((i * 2) + 1)
+    }
+
+    function getRight(i) {
+        return ((i * 2) + 2)
+    }
+
+    function swap(parentIndex, index) {
+        [heap[parentIndex], heap[index]] = [heap[index], heap[parentIndex]]
+    }
+
+    function insert(val) {
+        heap.push(val)
+        bubbleUp()
+        console.log(heap)
+    }
+
+    function extractMin() {
+        if (heap.length === 0) return null
+        if (heap.length === 1) return heap.pop()
+        let min = heap[0]
+
+        heap[0] = heap.pop()
+        bubbleDown()
+
+        console.log('Tree after min extraction', heap)
+        return min
+    }
+
+    function bubbleUp() {
+        let index = heap.length - 1
+
+        while (index > 0) {
+            let parentindex = getParent(index)
+            if (heap[parentindex] > heap[index]) {
+                swap(parentindex, index)
+                index = parentindex
+            } else {
+                break
+            }
+        }
+    }
+
+    function bubbleDown() {
+        let index = 0
+
+        while (getLeft(index) <= (heap.length - 1)) {
+            let left = getLeft(index)
+            let right = getRight(index)
+            let minValueIndex = left
+
+
+            if (right <= heap.length - 1 && heap[right] < heap[left]) {
+                minValueIndex = right
+            }
+
+            if (heap[index] > heap[minValueIndex]) {
+                swap(index, minValueIndex)
+                index = minValueIndex
+            } else {
+                break
+            }
+        }
+    }
+
+    console.log({ extractMin })
+    return { insert, extractMin }
+}
+
+
 /**
  * @param {string} s
  * @return {number}
