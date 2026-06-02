56ff68f3ff55865c5dd9be9b9b90c9cd9839be64
unknown <18756859+brandonholderman@users.noreply.github.com>
2026-06-01 23:11:55 -0700

    removed notes and added gitignore

---
+++ LeetCode/wordPattern.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+const wordPattern = function (pattern, s) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const result = {}
+
+
+    
+
+    alphabet.indexOf(pattern)
+};
